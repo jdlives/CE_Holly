978b3e70bfbcd4462dbc68945914c0af67062435
jdlives <jlaborada.97@gmail.com>
2018-02-06 18:40:48 +0800

    Add inverse function

--- matrix_operations.c
+++ matrix_operations.c
@@ -5,6 +5,7 @@
      */
 
     #include <stdio.h>
+    #include <math.h>
 
      
 
@@ -18,7 +19,11 @@
 
     void addition(int m1, int n1, float a[10][10], int m2, int n2, float b[10][10], float c[10][10]);
 
+    float determinant(float a[10][10], float k);
 
+    void cofactor(float num[10][10], float f);
+
+    void transpose(float num[10][10], float fac[10][10], float r);
 
      
 
@@ -26,7 +31,7 @@
 
     {
 
-        float a[10][10], b[10][10], c[10][10] = {0}, d[10][10] = {0};
+        float dt, a[10][10], b[10][10], c[10][10] = {0}, d[10][10] = {0}, e[10][10];
         int m1, n1, m2, n2, i, j, k;
 
         printf("Enter rows and columns for Matrix A respectively: ");
@@ -56,8 +61,21 @@
                 scanf("%f", &b[i][j]);
             }
 
-            subtraction(m1, n1, a, m2, n2, b, c);
+            // subtraction(m1, n1, a, m2, n2, b, c);
             diagonal(m1,n1,a,d);
+            /*Getting the inverse*/
+            dt = determinant(d, m1);
+
+            if (dt == 0)
+            {
+                printf("\nInverse of Entered Matrix is not possible\n");
+            }
+            else
+            {
+                cofactor(d, m1);
+            }
+            /*end*/
+
         }
         printf("On matrix multiplication of A and B the result is:\n");
         display(m1, n2, c);
@@ -158,6 +176,120 @@
             for (j = 0; j < n2; j++)
             {
                 printf("%f  ", c[i][j]);
+            }
+            printf("\n");
+        }
+    }
+
+    /*For calculating Determinant of the Matrix */
+
+    float determinant(float a[10][10], float k)
+
+    {
+        float s = 1, det = 0, b[10][10];
+        int i, j, m, n, c;
+        if (k == 1)
+        {
+            return (a[0][0]);
+        }
+        else
+        {
+            det = 0;
+            for (c = 0; c < k; c++)
+            {
+                m = 0;
+                n = 0;
+                for (i = 0;i < k; i++)
+                {
+                    for (j = 0 ;j < k; j++)
+                    {
+                        b[i][j] = 0;
+                        if (i != 0 && j != c)
+                        {
+                            b[m][n] = a[i][j];
+                            if (n < (k - 2))
+                            {
+                                n++;
+                            }    
+                            else
+                            {
+                                n = 0;
+                                m++;
+                            }
+                        }
+                    }
+                }
+                det = det + s * (a[0][c] * determinant(b, k - 1));
+                s = -1 * s;
+            }
+        }
+            return (det);
+    }
+    
+    void cofactor(float num[10][10], float f)
+    {
+        float b[10][10], fac[10][10];
+        int p, q, m, n, i, j;
+        for (q = 0;q < f; q++)
+        {
+            for (p = 0;p < f; p++)
+            {
+                m = 0;
+                n = 0;
+                for (i = 0;i < f; i++)
+                {
+                    for (j = 0;j < f; j++)
+                    {
+                        if (i != q && j != p)
+                        {
+                            b[m][n] = num[i][j];
+                            if (n < (f - 2))
+                            {
+                                n++;
+                            }
+                            else
+                            {
+                                n = 0;
+                                m++;
+                            }
+                        }
+                    }
+                }
+                fac[q][p] = pow(-1, q + p) * determinant(b, f - 1);
+            }
+        }
+        transpose(num, fac, f);
+    }
+
+    /*Finding transpose of matrix*/ 
+
+    void transpose(float num[10][10], float fac[10][10], float r)
+    {
+        int i, j;
+        float b[10][10], inverse[10][10], d;
+
+        for (i = 0;i < r; i++)
+        {
+            for (j = 0;j < r; j++)
+            {
+                b[i][j] = fac[j][i];
+            }
+        }
+        d = determinant(num, r);
+        for (i = 0;i < r; i++)
+        {
+            for (j = 0;j < r; j++)
+            {
+                inverse[i][j] = b[i][j] / d;
+            }
+        }
+        printf("\n\n\nThe inverse of matrix is : \n");
+
+        for (i = 0;i < r; i++)
+        {
+            for (j = 0;j < r; j++)
+            {
+                printf("\t%f", inverse[i][j]);
             }
             printf("\n");
         }
